8722bca2e2e932d6a7ea90d52f94decd197668c4
williamt <williamt@infront.net.au>
2016-05-11 16:31:02 +1000

    ssh module added, added deployment task for tomcat

--- hookr.rb
+++ hookr.rb
@@ -1,7 +1,7 @@
 require 'artii'
 require_relative 'lib/task_orchestrator'
 
-hooker_module = 'billing-frontend-deployment-bxtt'
+hooker_module = ''
 json = JSON.parse(File.read("../deployment_modules/#{hooker_module}.json"))
 task = TaskOrchestrator.new(json)
 task.process
